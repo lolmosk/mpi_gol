d554d9eb9ef57b76b68e4cfb270d2c8742cd0316
zooldoo <mironenkovb@gmail.com>
2019-02-07 21:16:53 +0300

    Add single process solution

--- MPI_PROJECT/Source.cpp
+++ MPI_PROJECT/Source.cpp
@@ -27,6 +27,71 @@ void PrintField(std::ostream& output, const std::vector<char>& field) {
 
 static_assert(field_height % N_THREADS == 0, "Incorrect field size");
 
+void SingleThread() {
+	std::fstream f;
+	constexpr int SEED = 1646868;
+	constexpr std::array<size_t, 8> HARDCODE_OFFSET =
+			{0,1,2,field_width+2,field_width+4,2*field_width+4,2*field_width+5,2*field_width+6};
+
+	int numprocs, myid;
+	int rc;
+
+	numprocs = 8;
+	myid = 0;
+
+	std::vector<char> field((field_width + 2) * (field_height + 2) + 1);
+	std::vector<char> field_history;
+
+	std::mt19937 engine(SEED);
+	std::uniform_int_distribution<> distribution(0, 1);
+
+	for (int i = 1; i <= field_height; ++i) {
+		for (int j = 1; j <= field_width; ++j) {
+			field[i * (field_width + 2) + j + 1] = distribution(engine);
+		}
+	}
+	field_history = field;
+	auto start = std::chrono::system_clock::now();
+
+	for (size_t generation = 0; generation < generations; ++generation) {
+		field_history = field;
+		std::vector<int> all_intermediate((field_width + 2) * field_height);
+
+		for (size_t i = 0; i < all_intermediate.size(); ++i) {
+			for (size_t proc = 0; proc < N_THREADS; ++ proc) {
+				all_intermediate[i] += (field[i + HARDCODE_OFFSET[proc]]);
+			}
+		}
+
+		for (size_t i = 0; i < all_intermediate.size(); ++i) {
+			if (i % (field_width + 2) == 0 || i % (field_width + 2) == (field_width + 1)) {
+				all_intermediate[i] = 0;
+			} else {
+				if (all_intermediate[i] == 3 || (all_intermediate[i] == 2 && field[(field_width + 2) * (myid + 1) + i + 1])) {
+					all_intermediate[i] = 1;
+				} else {
+					all_intermediate[i] = 0;
+				}
+			}
+		}
+
+		std::copy(std::begin(all_intermediate), std::end(all_intermediate), field.begin() + (field_width + 3));
+
+		// место в котором нужно останавливать игру, если достигнут конец. Но нужно остановить все процессы
+		field_history = field;
+		// вместо cout можно передать файловый поток
+		std::string filename(".\\dumps\\gen" + std::to_string(generation) + ".txt");
+		f.open(filename.c_str(), std::ios::out);
+		//PrintField(std::cout, field);
+		PrintField(f, field);
+		f.close();
+		std::cout << std::endl << std::endl << "GENERATION: " << generation << " ENDED" << std::endl << std::endl;
+	}
+	auto finish = std::chrono::system_clock::now();
+	std::cout << "Not parallel life: " << std::chrono::duration_cast<std::chrono::milliseconds>(finish - start).count() << "ms" << std::endl;
+	system("pause");
+}
+
 int main(int argc, char* argv[]) {
 	std::fstream f;
 	constexpr int SEED = 1646868;
